557a80ddb2b62ae261273278d487bd4d63b596c2
Andreas Hofreither <andreas@hofreither.at>
2026-06-26 08:20:39 +0200

    chore: prepare coolify deployment on develop

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   allowedDevOrigins: ["172.17.48.1"],
+  output: "standalone",
 };
 
 export default nextConfig;
